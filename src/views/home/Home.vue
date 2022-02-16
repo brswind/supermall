75adf4a597a7243f411003c9fccf23b0a42c1022
brswind <bairasnhi@126.com>
2022-02-16 11:30:34 +0800

    goods v1.0

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,72 +1,24 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
-    <Wrapper :cimgs="banners"/>
-    <RecommendView :recommends="recommends"/>
-    <TabControl :titles="titles" class="tab-control"/>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-    <h2>多个ul元素 快速增加</h2>
-
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+    <Wrapper :cimgs="banners" />
+    <RecommendView :recommends="recommends" />
+    <TabControl @tabClick="tabClick" :titles="titles" class="tab-control" />
+    <goods-list :goods="showGoods" />
   </div>
 </template>
 
 <script>
-import RecommendView from './childComps/RecommendView'
+import RecommendView from "./childComps/RecommendView";
 
-import NavBar from 'components/common/navbar/NavBar'
-import Wrapper from 'components/common/swiper/swiper'
-import TabControl from 'components/content/tabControl/TabControl'
+import NavBar from "components/common/navbar/NavBar";
+import Wrapper from "components/common/swiper/swiper";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
 
-import {getHomeMultidata, getHomeGoods} from 'network/home'
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
@@ -74,57 +26,102 @@ export default {
     RecommendView,
     NavBar,
     Wrapper,
-    TabControl
+    TabControl,
+    GoodsList
   },
   data() {
     return {
       banners: [],
       recommends: [],
-      titles: ['流行','新款','精选'],
+      titles: ["流行", "新款", "精选"],
       goods: {
-        'pop': {page: 0, list:[]},
-        'news': {page: 0, list:[]},
-        'sell': {page: 0, list:[]}
-      }
-
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      currentType: 'pop'
+    };
+  },
+  computed: {
+    showGoods(){
+      return this.goods[this.currentType].list
     }
   },
   created() {
     // 1. 请求多个数据
-    getHomeMultidata().then((result) => {
-      this.banners = result.data.banner.list
-      this.recommends = result.data.recommend.list
-      // console.log(this.banners)
-    }).catch((err) => {
-      console.log(err)
-    });
+    this.getHomeMultidata();
 
     // 2. 请求商品数据
-    getHomeGoods('pop',1).then((result) => {
-      console.log(result)
-    }).catch((err) => {
-      console.log(err)
-    });
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
   },
-}
+  methods: {
+    /**
+     * 网络请求相关的方法
+     */
+    getHomeMultidata() {
+      getHomeMultidata()
+        .then(result => {
+          this.banners = result.data.banner.list;
+          this.recommends = result.data.recommend.list;
+          // console.log(this.banners)
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page)
+        .then(result => {
+          const resList = result.data.list;
+          // console.log(resList);
+          this.goods[type].list.push(...resList);
+          this.goods[type].page += 1;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+
+    /**
+     * 事件监听相关的方法
+     */
+    tabClick(index){
+      switch(index){
+        case 0:
+          this.currentType='pop'
+          break
+        case 1:
+          this.currentType='new'
+          break
+        case 2:
+          this.currentType='sell'
+          break
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
-  #home {
-    padding-top: 44px;
-  }
-  .home-nav {
-    background-color: var(--color-tint);
-    color: #fff;
+#home {
+  padding-top: 44px;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
 
-    position: fixed;
-    left: 0;
-    right: 0;
-    top: 0;
-    z-index: 99;
-  }
-  .tab-control {
-    position: sticky;
-    top: 44px;
-  }
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 99;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
 </style>
